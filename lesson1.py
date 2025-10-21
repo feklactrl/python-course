db83a1a1f1d27b87f48b7375cdd184c82d4a1266
feklactrl <flepshina@mail.ru>
2025-10-21 11:03:34 +0300

    Первый коммит: добавлен lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+# name = "kit"
+# age = 18
+# favorite_activity = "тренировки в зале"
+#
+# # print("меня зовут", name)
+# # print("мой возраст", age)
+# # print("мое любимое занятие это", favorite_activity)
+# print(f"меня зовут {name}, мне {age} лет, мое любимое занятие это {favorite_activity}!")
+
+
+calories_per_day = 1800
+days = 5
+total_calories = calories_per_day * days
+print(f"всего за {days} сьедено {total_calories} калорий")
