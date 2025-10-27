b2e02aecd6ca8e3d8c7bf5e783615459eee0b822
feklactrl <flepshina@mail.ru>
2025-10-27 17:53:00 +0300

    обнова кода lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -1,16 +1,32 @@
 # дз 1
 
-fruits = ['apple', 'banana', 'avocado', 'orange', 'grape']
-for fruit in fruits:
-    print(fruit)
+# fruits = ['apple', 'banana', 'avocado', 'orange', 'grape']
+# for fruit in fruits:
+#     print(fruit)
+#
+#
+# # дз 2
+#
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+#
+# total = 0
+#
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(number)
+#
+#     total += number
+#
+# print(f"сумма всех чисел: {total}")
 
 
-# дз 2
+# дополнительные задания
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,5,7,6,8,9,3,4,2,6]
 total = 0
 for number in numbers:
     if number % 2 == 0:
         print(number)
-
-print("сумма всех чисел: ", total)
+    min([number])
+    max([number])
+    print(f"минимальное: {min}, максимальное: {max}")
