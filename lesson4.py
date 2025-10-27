7a727d78ce26f270c7c889f6bcb54e3cd8290da3
feklactrl <flepshina@mail.ru>
2025-10-27 17:29:36 +0300

    добавлен lesson.py

---
+++ lesson4.py
@@ -0,0 +1,16 @@
+# дз 1
+
+fruits = ['apple', 'banana', 'avocado', 'orange', 'grape']
+for fruit in fruits:
+    print(fruit)
+
+
+# дз 2
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+total = 0
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+print("сумма всех чисел: ", total)
