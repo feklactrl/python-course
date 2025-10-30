2a434f80b7bb00c95306f6f7c57ad98cf2085203
feklactrl <flepshina@mail.ru>
2025-10-30 12:39:37 +0300

    добавлен lesson6.py

---
+++ lesson6.py
@@ -0,0 +1,43 @@
+#примеры
+
+# person = {
+#     "name": "Fekla",
+#     "age": 18,
+#     "favorite_activity": "танцы"
+# }
+#
+# print("имя:", person['name'])
+# print("возраст:", person['age'])
+# print("занятие:", person['favorite_activity'])
+#
+#
+# person["hobby"] = "вязание"
+# person["age"] = 19
+#
+# print("обновленный словарь:", person)
+#
+# del person["favorite_activity"]
+# print("после удаления:", person)
+
+
+# дз 1
+pet = {
+    "name": "Марсэль",
+    "type": "собака",
+    "age": 2
+}
+pet["favorite_food"] = "сыр"
+pet["age"] = 3,
+del pet["type"]
+
+
+#дз 2
+
+student = {
+    "name": "Cris",
+    "age": 28,
+    "subjects": "танцы, вокал"
+}
+print("имя:", student['name'])
+print("возраст:", student['age'])
+print("занятия:", student['танцы'])
