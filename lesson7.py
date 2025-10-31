04b7e916ab0143df88daf83813afc7509c1b743f
feklactrl <flepshina@mail.ru>
2025-10-31 15:43:28 +0300

    добавлен lesson7.py

---
+++ lesson7.py
@@ -0,0 +1,32 @@
+# def greet():
+#     print("привет, как дела?")
+# greet()
+#
+# def greet(name):
+#     print(f"привет, {name}!")
+# greet("Kit")
+#
+# def add(a, b):
+#     return a + b
+# result = add(5,3)
+# print(result)
+
+#дз 1
+
+def hello_user(name):
+    print(f"привет, {name}!")
+hello_user("Марсэль")
+
+# дз 2
+
+def square(num):
+    return num ** 2
+result = square(25)
+print(result)
+
+# дз 3
+
+def sum_numbers(a, b, c):
+    return a + b + c
+result = sum_numbers(3, 1, 7)
+print(result)
